46dba541f324c3b635c26edd41040c9666f131ed
KelvinEztech <Krusher@eztech.ind.br>
2018-09-07 00:03:42 -0300

    26b01A2

--- Product/EZInstallWec/RestoreDB.cmd
+++ Product/EZInstallWec/RestoreDB.cmd
@@ -1,11 +1,11 @@
-eznet stop all
+EZNet stop all
 cd \NDFlash\SQLite
 del EzDBOld.SQL
 ren EzDB.SQL EzDBOld.SQL
 cd \
 del *.zip 
 del *.L00
-eznet start all
-ezextract import config.ini 
+EZNet start all
+EZExtract import \NDFlash\SQLite\config.ini 
 exit
 
