b4ad1bf557513db812cd2107567518b29ec70b1a
KelvinEztech <Krusher@eztech.ind.br>
2019-12-16 15:01:20 -0300

    WIP 16/12/2019

---
+++ Product/EZInstallWec/InitBBB.bat
@@ -0,0 +1,9 @@
+@echo off
+c:
+cd \Vision
+del ini\EZProductGroup.ini
+EZTelnet\EZTelnet -C InitBBBBefore.cmd -O logs\InitBBBBefore.log 
+EZTelnet\EZTelnet -C WriteProtect.cmd -O logs\WriteProtect.log 
+EZTelnet\EZTelnet -C InitBBB.cmd -O logs\Upgrade.log 
+exit_upgrade:
+
---
+++ Product/EZInstallWec/InitBBB.cmd
@@ -0,0 +1,7 @@
+setdate
+settime
+eznet service add ezserver 30
+eznet service add ez2serial 31
+eznet service add ezhowsc 32
+eznet start all
+exit
