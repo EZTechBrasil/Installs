d039117a00a8119afb11b4bb0fe73abcffc5c9bc
KelvinEztech <krusher@eztech.ind.br>
2022-12-08 17:26:59 -0300

    25b18b9 installs, added connectec driver installation

--- Product/EZInstallCE/Start.cmd
+++ Product/EZInstallCE/Start.cmd
@@ -1,6 +1,11 @@
 setdate
 settime
 ezloader EZMOD
+eznet stop EZHOWSC
+attrib -R \NDFlash\EZForecourt\EZHOWSC.*
+del \NDFlash\EZForecourt\EZHOWSC.*
 attrib -R \NDFlash\EZForecourt\*.ini
+attrib +R \NDFlash\EZForecourt\EZConnect.dll
+ezhowscinst /EZC /DNS 
 eznet start all
 exit
--- Product/EZInstallWec/Start.cmd
+++ Product/EZInstallWec/Start.cmd
@@ -4,6 +4,10 @@ settime
 ezloader EZMOD
 eznet service add ezserver 30
 eznet service add ez2serial 31
-eznet service add ezhowsc 32
-eznet start all
+eznet service del EZHOWSC
+attrib -R \NDFlash\EZForecourt\EZHOWSC.*
+del \NDFlash\EZForecourt\EZHOWSC.*
+ezhowscinst /EZC /DNS 
+eznet service add EZConnect 32
+eznet start all 
 exit
