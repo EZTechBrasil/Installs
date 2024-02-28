da94a4c9bc0aa4370555435db3ff6cb34d7977da
KelvinEztech <krusher@eztech.ind.br>
2024-02-28 10:28:06 -0300

    Version 25b80 B9 Release

--- Product/CopyVersion.bat
+++ Product/CopyVersion.bat
@@ -6,6 +6,7 @@ pause
 copy \Vision\Release\*.B2G \zeztech\Release\Product\EZInstallWec\Release
 copy \Vision\Release\*.exe \zeztech\Release\Product\EZInstallWec\Release
 copy \Vision\Release\*.dll \zeztech\Release\Product\EZInstallWec\Release
+copy \WINCE800\Osdesign\AM33X_BBB8\AM33X_BBB8\RelDir\AM33X_BBB_BSP_ARMV7_Release\Wec*.exe \zeztech\Release\Product\EZInstallWec\EZApps
 pause 
 copy \Work200\EZInstall\*.B2G \zeztech\Release\Product\EZInstallXP
 copy \Work200\EZInstall\*.ARM \zeztech\Release\Product\EZInstallXP
@@ -16,3 +17,12 @@ xcopy \Work200\EZInstall\EZmonitor\* \zeztech\Release\Product\EZInstallXP\EZmoni
 xcopy \Work200\EZInstall\x64\* \zeztech\Release\Product\EZInstallXP\x64 /s /y
 xcopy \Work200\EZInstall\Passo\* \zeztech\Release\Product\EZInstallXP\Passo /s /y
 pause 
+
+
+copy \Work200\EZServer\Linux\Release\ezserver \zeztech\Release\Product\Linux64 
+copy \Work200\EZExtract\Linux\Release\ezextract \zeztech\Release\Product\Linux64 
+copy \Work200\EZClientDLL\Linux64\Release\EZClient.so.1 \zeztech\Release\Product\Linux64 
+copy \Work200\EZClientDLL\Linux\Release\EZClient.so.1 \zeztech\Release\Product\Linux 
+copy \Work200\EZClientDLL\Centos7\Release\EZClient.so.1 \zeztech\Release\Product\Centos7 
+copy \Work200\EZClientDLL\Centos6\Release\EZClient.so.1 \zeztech\Release\Product\Centos6 
+pause
