ccb035beb23f93c2e92a982e1873fd4043e7b1df
KelvinEztech <krusher@eztech.ind.br>
2025-01-07 16:29:42 -0300

    25b80 B19 Release

--- Product/EZInstallCE/Logs.cmd
+++ Product/EZInstallCE/Logs.cmd
@@ -1,5 +1,5 @@
 del config.ini
 ezextract export config.ini 
 copy config.ini \NDflash\SQLite
-ezzip -c Logs.zip *L??.zip *.l?? *.ini \ndflash\log\*.* \NDFLash\EZForecourt\Tank*.ini
+ezzip -c Logs.zip *L??.zip *.l?? *.ini \ndflash\log\*.* \NDFLash\EZForecourt\*.ini
 exit
